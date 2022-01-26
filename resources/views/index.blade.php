23772e263e164ca0b682c5f48fabb7528d566b16
小林空晏 <katakuriko229@kobayashisoraannoMacBook-Pro.local>
2022-01-26 20:30:50 +0900

    Initialize repository

---
+++ resources/views/index.blade.php
@@ -0,0 +1,146 @@
+<x-layout>
+    <x-slot name="title">
+        ESA ACADEMY 生徒管理システム
+    </x-slot>
+    <div class="main_wrapper">
+      <p class="sum_number">15件</p>
+      <div class="main_content">
+        <table>
+          <tr>
+            <th>名前</th>
+            <th>年齢</th>
+            <th>生年月日</th>
+            <th>e-mail</th>
+            <th>TEL</th>
+            <th>プラン名</th>
+          </tr>
+          <tr>
+            <td>鈴木 健司</td>
+            <td>20</td>
+            <td>2001/9/21</td>
+            <td>suzuken@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>PREMIUM</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>山田 可奈子</td>
+            <td>22</td>
+            <td>1999/12/6</td>
+            <td>kanako-1206@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>PREMIUM</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>松田 隆</td>
+            <td>19</td>
+            <td>2002/1/10</td>
+            <td>matsusda-matsu@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>STANDARD</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>山田 秀幸</td>
+            <td>20</td>
+            <td>2001/9/21</td>
+            <td>hide-0001@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>PREMIUM</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>森 直子</td>
+            <td>22</td>
+            <td>1999/12/6</td>
+            <td>naoko-mori@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>PREMIUM</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>高岡 正和</td>
+            <td>19</td>
+            <td>2001/1/10</td>
+            <td>mskz0110@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>STANDARD</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>坂本 優</td>
+            <td>20</td>
+            <td>2001/9/21</td>
+            <td>suguru@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>PREMIUM</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>大島 美香子</td>
+            <td>22</td>
+            <td>1999/12/6</td>
+            <td>mikako01234@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>PREMIUM</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>山本 志帆</td>
+            <td>19</td>
+            <td>2002/1/10</td>
+            <td>yamashiho2002@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>STANDARD</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option btn_option">削除</div>
+            </td>
+          </tr>
+          <tr>
+            <td>丸山 宏二</td>
+            <td>20</td>
+            <td>2001/9/21</td>
+            <td>koji0921@ggmail.com</td>
+            <td>080-1234-5678</td>
+            <td>PREMIUM</td>
+            <td>
+              <div class="edit_btn btn btn_option">編集</div>
+              <div class="delete_btn btn btn_option btn_option">削除</div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div class="pagers">
+      <a href="#"><div class="pager active btn_option">1</div></a>
+      <a href="#"><div class="pager btn_option">2</div></a>
+      <a href="#" class="next"> <i class="fas fa-angle-right"></i> </a>
+    </div>
+</x-layout>
+
